49e50a05003e96bd93cef5a29a7b38e3ee63eb06
Yinhao <1454135273@qq.com>
2019-08-01 16:19:33 +0930

    add work

--- neuralNetwork_class.py
+++ neuralNetwork_class.py
@@ -1,12 +1,12 @@
 import numpy
-#scipy.special for the sigmoid function expit()
+# scipy.special for the sigmoid function expit()
 import scipy.special
 
-#neural network class definition
+# neural network class definition
 class neuralNetwork:
-    #initialise the neural network
+    # initialise the neural network
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
-        #set number of nodes in each input, hidden, output, layer
+        # set number of nodes in each input, hidden, output, layer
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
@@ -20,16 +20,29 @@ class neuralNetwork:
         self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
         self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
         
-        #learning rate
+        # learning rate
         self.lr = learningrate
         
-        #activation function is the sigmoid function
-        self.activation_function = lambda x:scipy.special.expit(x)
-        
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
         pass
         
-    
-    def train():
+    #train the neural network
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin = 2).T
+        targets = numpy.array(targets_list, ndmin = 2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calulatesignals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
         pass
     
     #query the neural network    
@@ -48,4 +61,12 @@ class neuralNetwork:
         final_outputs = self.activation_function(final_inputs)
         
         return final_outputs
-        
+
+
+#-------------------------------------------------------------------------------
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learning_rate = 0.3
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+n.query([1.0, 0.5, -1.5])
