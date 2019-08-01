27a4f612cace2eee4d65edb188b88733ccaebbbd
Yinhao <1454135273@qq.com>
2019-08-01 11:51:00 +0930

    Update neuralNetwork_class.py

--- neuralNetwork_class.py
+++ neuralNetwork_class.py
@@ -12,14 +12,14 @@ class neuralNetwork:
         self.onodes = outputnodes
         
         """
-        link weigh matrices, wih and who
+        link weight matrices, wih and who
         weights inside the arrays are w_i_j, where link is from nonde i to node j in the next layer
         w11 w21
         w12 w22 etc
         """
         self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes)
-                                       
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        
         #learning rate
         self.lr = learningrate
         
